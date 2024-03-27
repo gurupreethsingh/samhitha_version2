df52cf83ed4c73fa397ef5d3becb35f6cb416991
Gurupreeth <igurupreet@gmail.com>
2024-03-27 14:19:31 +0530

    core java project and normal automation project added

---
+++ core_java_topics/src/datatypes_variables/Helloworld.java
@@ -0,0 +1,10 @@
+package datatypes_variables;
+
+public class Helloworld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello world");
+  
+	}
+
+}
---
+++ samhitha_automation_scripts/src/opening_browsers/OpenChromeBrowser.java
@@ -0,0 +1,10 @@
+package opening_browsers;
+
+public class OpenChromeBrowser {
+
+	public static void main(String[] args) {
+		System.out.println("Open chrome browser");
+
+	}
+
+}
